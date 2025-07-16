55b8770b0d6ca38416df13ae938a6b20501dd8a5
Guido Warken <Guido Filho>
2025-07-15 22:47:06 -0300

    [Aula 3 atividade 3]:: Altera o program.cs para transformar resposta json em lista de músicas

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,10 +1,14 @@
 ﻿
+using ScreenSound_04.Modelos;
+using System.Text.Json;
+
 using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[0].ExibirFichaTecnica();
     }
     catch (Exception ex)
     {
