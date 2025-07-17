8c4673ae5a4c29c81a7ed77905022058a79ea114
Guido Filho <8114@tcesc.tc.br>
2025-07-17 09:32:59 -0300

    [Aula 4 aividade 3]: Instancia e exibe as músicas favoritas

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -9,10 +9,29 @@ using (HttpClient client = new HttpClient())
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        var musicasPreferidasDoDaniel = new MusicasPreferidas("Daniel");
+        musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1]);
+        musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[377]);
+        musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[4]);
+        musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[6]);
+        musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1467]);
+
+        musicasPreferidasDoDaniel.ExibirMusicasFavoritas();
+
+        var musicasPreferidasEmilly = new MusicasPreferidas("Emy");
+
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[500]);
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[637]);
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[428]);
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[13]);
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[71]);
+
+        musicasPreferidasEmilly.ExibirMusicasFavoritas();
         //musicas[0].ExibirFichaTecnica();
         //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
         //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas);
-        LinqFilter.FiltrarMusicasPorArtista(musicas);
+        //LinqFilter.FiltrarMusicasPorArtista(musicas);
         //LinqOrder.ExibirListaDeArtistasFiltrados(musicas);
     }
     catch (Exception ex)
